c4117ec8e5112af8eac4a57c58157ba6d382b31f
happyfridaycode <30129914+happyfridaycode@users.noreply.github.com>
2020-12-30 21:28:35 -0500

    Updates log files

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.springboot;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.InetAddress;
+
+
+@Slf4j
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			String fqdn = address.getCanonicalHostName();
+			return String.format("Greetings from %s (%s)", fqdn, address.getHostAddress());
+		} catch (Throwable t) {
+			log.error("failed to get local addresses",t);
+		}
+		return "Greetings from guhada service";
 	}
+
+
+
 
 }
