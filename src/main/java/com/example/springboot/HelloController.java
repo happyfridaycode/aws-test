4e0fd95bcd45c267c4ff89b8c9275993e027c6da
happyfridaycode <30129914+happyfridaycode@users.noreply.github.com>
2021-01-05 03:32:46 -0500

    revert

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,12 +3,27 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.InetAddress;
+
+
+//@Slf4j
 @RestController
 public class HelloController {
-
+	private final static String VERSION = "v1";
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			String fqdn = address.getCanonicalHostName();
+			return String.format("Greetings from GUHADA service (%s): %s-%s",
+					VERSION, fqdn, address.getHostAddress());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+		return String.format("Greetings from GUHADA service (%s) ", VERSION);
 	}
+
+
+
 
 }
