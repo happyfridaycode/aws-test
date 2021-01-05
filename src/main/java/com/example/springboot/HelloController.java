933b7d5523bac782c9c2411b3fc50ef08b84e7f6
happyfridaycode <30129914+happyfridaycode@users.noreply.github.com>
2021-01-05 05:05:39 -0500

    remove logs

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,12 @@
 package com.example.springboot;
 
-import lombok.extern.log4j.Log4j;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 
 
-@Slf4j
+
 @RestController
 public class HelloController {
 	private final static String VERSION = "v1";
@@ -20,7 +18,7 @@ public class HelloController {
 			return String.format("Greetings from GUHADA service (%s): %s-%s",
 					VERSION, fqdn, address.getHostAddress());
 		} catch (Throwable t) {
-			log.error("failed to get local addresses",t);
+t.printStackTrace();
 		}
 		return String.format("Greetings from GUHADA service (%s) ", VERSION);
 	}
